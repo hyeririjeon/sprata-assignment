931e7002c29085ddbb1c6de2fc69c1dbd8289e8d
hyeririjeon <jeonhyeri5555@gmail.com>
2025-05-14 14:44:12 +0900

    :white_check_mark: test: 비밀번호 암호화 테스트 작성

--- src/test/java/com/study/usermanagementsystem/service/CreateUserServiceTest.java
+++ src/test/java/com/study/usermanagementsystem/service/CreateUserServiceTest.java
@@ -2,12 +2,15 @@ package com.study.usermanagementsystem.service;
 
 import com.study.usermanagementsystem.common.exception.UserException;
 import com.study.usermanagementsystem.common.response.UserStatusCode;
+import com.study.usermanagementsystem.domain.User;
 import com.study.usermanagementsystem.dto.request.SignUpRequestDto;
 import com.study.usermanagementsystem.dto.response.SignUpResponseDto;
 import com.study.usermanagementsystem.repository.InMemoryUserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -15,10 +18,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class CreateUserServiceTest {
 
     private CreateUserService createUserService;
+    private InMemoryUserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
     @BeforeEach
     void setUp() {
-        createUserService = new CreateUserService(new InMemoryUserRepository());
+        userRepository = new InMemoryUserRepository();
+        passwordEncoder = new BCryptPasswordEncoder();
+        createUserService = new CreateUserService(userRepository, passwordEncoder);
     }
 
     @Test
@@ -48,4 +55,20 @@ class CreateUserServiceTest {
         ).isInstanceOf(UserException.class)
                 .hasMessage(UserStatusCode.USER_ALREADY_EXISTS.getMessage());
     }
+
+    @Test
+    @DisplayName("비밀번호 암호화 확인")
+    void password_should_be_encoded() {
+        // given
+        SignUpRequestDto request = new SignUpRequestDto("test", "1234", "test");
+        createUserService.register(request);
+
+        // when
+        User savedUser = userRepository.findByLoginId("test").orElseThrow();
+
+        // then
+        assertThat(savedUser.getPassword()).isNotEqualTo("1234");
+        assertThat(passwordEncoder.matches("1234", savedUser.getPassword())).isTrue();
+    }
+
 }
